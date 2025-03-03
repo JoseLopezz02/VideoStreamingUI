6707800839577d4daeaf371bbc24e0fa3c49b264
JoseLopezz02 <jjoselopezbt@gmail.com>
2025-03-03 15:39:46 +0100

    correction on error sintaxis

--- src/pages/AuthTokenGetSubscriptions.jsx
+++ src/pages/AuthTokenGetSubscriptions.jsx
@@ -6,7 +6,7 @@ export default function AuthTokenGetSubscriptions() {
   const [subscriptions, setSubscriptions] = useState([]);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("authToken");
 
     if (!token) {
       navigate("/");
